30b37be82a4c258a9815a4bc22a0e661dfefb21d
Juan Carlos <juan.delgadog@uanl.edu.mx>
2025-11-12 08:43:39 -0600

    Laboratorio 3

---
+++ Laboratorio 3.R
@@ -0,0 +1,69 @@
+#dbh esel diametro de altura de pecho
+dbh <- c(16.5, 25.3, 22.1, 17.2, 16.1, 8.1, 34.3, 5.4, 5.7, 11.2, 24.1,
+         14.5, 7.7, 15.6, 15.9, 10, 17.5, 20.5, 7.8, 27.3,
+         9.7, 6.5, 23.4, 8.2, 28.5, 10.4, 11.5, 14.3, 17.2, 16.8)
+#tree son los arboles de pino pero con una secuencia de numeros desde el 1 hasta el 30 (1,2,3,4,5...)
+tree <- seq(1,30)
+#En las parcelas estoy indicando cuantas columnas y filas quiero con la funcion "gl"
+parcelas <- gl(3,10)
+#Con "Trees" estoy creando un data.frame con las 3 variables anteriores
+Trees <- data.frame(tree,dbh,parcelas)
+
+
+url <- ("https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv")
+inspeccion <- read.csv(url)
+head(inspeccion)  
+
+
+
+prof_url_2 <- paste0("https://repodatos.atdt.gob.mx/api_update/senasica/",
+                     "actividades_inspeccion_movilizacion/",
+                     "29_actividades-inspeccion-movilizacion.csv")
+
+
+
+senasica <- read.csv(prof_url_2)
+head(senasica)
+
+library(repmis) 
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1") ##########
+
+
+
+
+head(conjunto) # muestra las primeras seis filas de la BD
+library(readr)
+file <- paste0("https://raw.githubusercontent.com/mgtagle/",
+               "202_Analisis_Estadistico_2020/master/cuadro1.csv")
+inventario <- read.csv(file)
+head(inventario)
+mean(Trees$dbh)
+sd(Trees$dbh)
+sum(Trees$dbh < 10)
+which(Trees$dbh < 10)
+trees.13 <- Trees[!(Trees$parcela=="2"),]
+trees.13
+trees.1 <- subset(trees, dbh <= 10)
+head(trees.1)
+mean(Trees$dbh)
+
+
+
+
+
+mamiferos <- read.csv("https://www.openintro.org/data/csv/mammals.csv")
+hist(mamiferos$total_sleep)
+
+hist(mamiferos$total_sleep, # Datos
+     xlim = c(0,20), ylim = c(0,14), 
+     main = "Total de horas sueño de las 39 especies", 
+     xlab = "Horas sueño", 
+     ylab = "Frecuencia", 
+     las = 1, 
+     col = "navajowhite")
+data("chickwts")
+head(chickwts[c(1:2,42:43, 62:64), ])
+feeds <- table(chickwts$feed)
+feeds
+barplot(feeds)
+barplot(feeds[order(feeds, decreasing = TRUE)])
